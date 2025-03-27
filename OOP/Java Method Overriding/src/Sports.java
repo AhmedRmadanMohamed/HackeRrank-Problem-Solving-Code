06fde8ad2664c402194ee4ff909361fb903e1b45
AhmedRamadanMohamed <ahmedramadanmohamedsmaha@gmail.com>
2025-03-27 08:53:52 +0200

    commit Java Method Overriding

---
+++ OOP/Java Method Overriding/src/Sports.java
@@ -0,0 +1,4 @@
+public class Sports {
+    public  String getName (){return  "Generic Sports";}
+    public void  getNumberOfTeamMembers(){System.out.println("Each team has n players in " + getName());}
+}
